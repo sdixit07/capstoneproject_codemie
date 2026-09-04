0cf8f60c2c0b39292f56778e090d9448737efc86
Saurabh Dixit <43204978+sdixit07@users.noreply.github.com>
2026-09-04 21:38:00 +0530

    feat(products): add JPA specification/paging support

--- ecom-project/src/main/java/org/ecom/productcatalog/repository/ProductRepository.java
+++ ecom-project/src/main/java/org/ecom/productcatalog/repository/ProductRepository.java
@@ -2,11 +2,9 @@ package org.ecom.productcatalog.repository;
 
 import org.ecom.productcatalog.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategoryId(Long categoryId);
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 }
